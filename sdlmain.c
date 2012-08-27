9b1f442b3be79f5034b8ba8ec80f5f9bbb053423
Brian Taylor <el.wubo@gmail.com>
2012-08-26 22:14:19 -0400

    stop initializing audio

--- sdlmain.c
+++ sdlmain.c
@@ -23,7 +23,7 @@ void shutdown() {
 }
 
 int main(int argc, char ** argv) {
-  if ( SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO) < 0 ) {
+  if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
     fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
     exit(1);
   }
