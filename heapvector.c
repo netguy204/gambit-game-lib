21c2b6d2639f57839a160eebcf7d0cae4433e661
Brian Taylor <el.wubo@gmail.com>
2012-10-28 10:06:07 -0400

    hv off by one error for small initial allocs

--- heapvector.c
+++ heapvector.c
@@ -5,7 +5,6 @@
 
 HeapVector heapvector_make(size_t init_alloc) {
   size_t size = sizeof(struct HeapVector_) + init_alloc;
-  size = NEXT_ALIGNED_SIZE(size);
 
   HeapVector result = malloc(size);
   result->data_bytes = 0;
@@ -25,8 +24,7 @@ HeapVector heapvector_push(HeapVector hv, void * data, size_t size) {
   size_t new_size = hv->data_bytes + size;
 
   if(new_size > hv->alloc_bytes) {
-    size_t new_alloc = sizeof(struct HeapVector_) + (hv->alloc_bytes * 2);
-    new_alloc = NEXT_ALIGNED_SIZE(new_alloc);
+    size_t new_alloc = sizeof(struct HeapVector_) + (new_size * 2);
 
     // i'm not sure this is correct. I used to get crashes here until
     // I aligned the allocation sizes...
