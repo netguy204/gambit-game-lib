5f5c41712d478c7bfec42e9be567d1a07735add7
Brian Taylor <el.wubo@gmail.com>
2013-01-01 08:33:54 -0500

    forgot to check in hdrs

---
+++ gl_headers.h
@@ -0,0 +1,31 @@
+/* the location of the GL headers is platform dependant. clean that up
+   a bit */
+
+#ifndef GLHEADERS_H
+#define GLHEADERS_H
+
+#ifdef BUILD_SDL
+#ifdef __APPLE__
+/* SDL on Mac */
+#include <OpenGL/gl.h>
+#else
+/* SDL on linux */
+#include <GL/glew.h>
+#include <GL/gl.h>
+#endif
+#define glOrthof glOrtho
+
+#else
+#ifdef BUILD_RPI
+/* Rpi */
+#include "GLES2/gl2.h"
+#include "EGL/egl.h"
+#include "EGL/eglext.h"
+
+#else
+
+#error "Either BUILD_SDL or BUILD_RPI must be defined"
+#endif
+#endif
+
+#endif
